e7e293246bc12ebf781b2d271fd47906e9d3cd76
Christopher <cli@biwise.dk>
2024-12-03 08:11:31 +0100

    Day 3 part 2 done, had to remove newlines in input

--- AdventOfCode2024/DayClasses/Aoc03DayLogic.cs
+++ AdventOfCode2024/DayClasses/Aoc03DayLogic.cs
@@ -54,7 +54,39 @@ namespace AdventOfCode2024.DayClasses
 
         public long RunQuestion2(FileInfo file, bool isBenchmark = false)
         {
-            throw new NotImplementedException();
+            var reader = new CleanFileReader();
+            var content = reader.GetReadableFileContent(file, isBenchmark);
+            content = "do()" + content;
+
+            var doPattern = @"(do\(\)|don't\(\))(((?!(do\(\)|don't\(\))).)*)";
+            var doRegex = new Regex(doPattern);
+            long result = 0;
+            foreach (Match match in doRegex.Matches(content))
+            {
+                if (match.Groups[1].Value == "do()")
+                {
+                    result += FindAndAddMatches(match.Captures[0].Value);
+                }
+            }
+            return result;
+        }
+
+        private long FindAndAddMatches(string content)
+        {
+            string pattern = @"mul\(\d{1,3},\d{1,3}\)";
+            var reg = new Regex(pattern);
+            var matches = reg.Matches(content);
+
+            var subPattern = @"\((\d{1,3}),(\d{1,3})\)";
+            var subRegex = new Regex(subPattern);
+            Match subMatch;
+            long result = 0;
+            foreach (var match in matches)
+            {
+                subMatch = subRegex.Match(match.ToString()!);
+                result += int.Parse(subMatch.Groups[1].Value) * int.Parse(subMatch.Groups[2].Value);
+            }
+            return result;
         }
     }
 }
